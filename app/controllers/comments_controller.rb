8cccee77d1daa8338062cf5cdf53266407cdf175
dantle1 <dantle@ucsb.edu>
2024-11-01 10:54:38 -0700

    fixed comments, added banned words

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    # @post = Post.find(params[:article_id])
-    @comment = Comment.new(comment_params)
-    # @post.comments.append(@comment)
-    # @comment = @post.comments.create(comment_params)
-    if @comment.save       
-      endflash[:success] = "Comment saved." 
-    end
-    # redirect_to post_path(@post)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.save
+    redirect_to post_path(@post)
   end
 
   def new
@@ -16,6 +12,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :post_id)
     end
 end
